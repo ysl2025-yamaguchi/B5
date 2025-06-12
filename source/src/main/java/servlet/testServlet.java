9a15cc7a5478f30757227cbe38db81170cc2f2ae
plusdojo2025-kikuura <kikuura-santaro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 11:57:07 +0900

    共有テスト

---
+++ source/src/main/java/servlet/testServlet.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class testServlet
+ */
+@WebServlet("/testServlet")
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
