628a54272c9f3643309f73da7802d3229a5b2c62
plusdojo2025-sekiguchi-yuta <sekiguchi-yuta-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 17:02:00 +0900

    Update CheeseMusicPhrase.java

--- source/src/main/java/dto/CheeseMusicPhrase.java
+++ source/src/main/java/dto/CheeseMusicPhrase.java
@@ -4,5 +4,79 @@ import java.io.Serializable;
 
 public class CheeseMusicPhrase  implements Serializable{
 	
+	private int id;
+	private int musicId;
+	private int phraseId;
+	private String title;
+	private String remarks;
+	private int order;
+	private String updatedAt;
+	private String createdAt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMusicId() {
+		return musicId;
+	}
+	public void setMusicId(int musicId) {
+		this.musicId = musicId;
+	}
+	public int getPhraseId() {
+		return phraseId;
+	}
+	public void setPhraseId(int phraseId) {
+		this.phraseId = phraseId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRemarks() {
+		return remarks;
+	}
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+	public int getOrder() {
+		return order;
+	}
+	public void setOrder(int order) {
+		this.order = order;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
 
+public CheeseMusicPhrase() {
+	this(0, 0, 0, "", "", 0, "", "");
 }
+	
+public CheeseMusicPhrase(int id, int musicId, int phraseId, String title, String remarks, int order,
+		String updatedAt, String createdAt) {
+	super();
+	this.id = id;
+	this.musicId = musicId;
+	this.phraseId = phraseId;
+	this.title = title;
+	this.remarks = remarks;
+	this.order = order;
+	this.updatedAt = updatedAt;
+	this.createdAt = createdAt;
+}
+}
+
