7a8a1bdf6ef2cfe70c682416f3a25c7e03a13707
plusdojo2025-kikuura <kikuura-santaro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 17:00:50 +0900

    CheesePhraseDAO.javaの更新

--- source/src/main/java/dao/CheesePhraseDAO.java
+++ source/src/main/java/dao/CheesePhraseDAO.java
@@ -1,5 +1,14 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.CheesePhrase;
+
 public class CheesePhraseDAO {
-	
+	public List<CheesePhrase> search(List<String> searchWordList, List<String> searchTagList) {
+		List<CheesePhrase> phraseList = new ArrayList<CheesePhrase>();
+		
+		return phraseList;
+	}
 }
