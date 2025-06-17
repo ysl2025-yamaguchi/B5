7327550638244a7fc625c0851cef53681d0a929e
plusdojo2025-sekiguchi-yuta <sekiguchi-yuta-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 16:44:19 +0900

    Update CheeseEditMusicServlet.java

--- source/src/main/java/servlet/CheeseEditMusicServlet.java
+++ source/src/main/java/servlet/CheeseEditMusicServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.naming.spi.DirStateFactory.Result;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -42,16 +41,15 @@ public class CheeseEditMusicServlet  extends HttpServlet {
 		CheeseMusicPhraseDao bDao = new CheeseMusicPhraseDao();
 		if (request.getParameter("submit").equals("更新")) {
 			if (bDao.update(new CheeseMusicPhrase(id, music_id, phrase_id, title, remarks, phrase_order, updated_at, created_at))) { // 更新成功
-				request.setAttribute("result", new Result("更新成功！", "レコードを更新しました。", "/B5/CheeseEditMusicServlet"));
+				request.setAttribute("result", "レコードを登録しました。");
 			} else { // 更新失敗
-				request.setAttribute("result", new Result("更新失敗！", "レコードを更新できませんでした。", "/webappAns/MenuServlet"));
+				request.setAttribute("result", "失敗");
 			}
 		} else {
-			if (bDao.delete(new Bc(number, company,  department, position, name, zipcode, address, 
-					phone, fax, email, remarks))) { // 削除成功
-				request.setAttribute("result", new Result("削除成功！", "レコードを削除しました。", "/webappAns/MenuServlet"));
+			if (bDao.delete(new CheeseMusicPhrase(id, music_id, phrase_id, title, remarks, phrase_order, updated_at, created_at))) { // 削除成功
+				request.setAttribute("result", "レコードを削除しました。");
 			} else { // 削除失敗
-				request.setAttribute("result", new Result("削除失敗！", "レコードを削除できませんでした。", "/webappAns/MenuServlet"));
+				request.setAttribute("result", "失敗");
 			}
 		}
 	}
