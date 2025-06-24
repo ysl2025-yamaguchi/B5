03cf94ecdbcd87b5227740966ba77bbff43e2d58
plusdojo2025-sekiguchi-yuta <sekiguchi-yuta-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-24 11:01:41 +0900

    曲編集画面の変更

--- source/src/main/java/servlet/CheeseEditMusicServlet.java
+++ source/src/main/java/servlet/CheeseEditMusicServlet.java
@@ -12,13 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import dao.CheeseMusicDao;
 import dao.CheeseMusicPhraseDao;
 import dao.CheesePhraseDao;
-import dto.CheeseMusic;
 import dto.CheeseMusicPhrase;
 import dto.CheesePhrase;
-import dto.CheeseUser;
 
 
 @WebServlet("/CheeseEditMusicServlet")
@@ -27,34 +24,29 @@ public class CheeseEditMusicServlet  extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		CheeseUser user = (CheeseUser)session.getAttribute("loginUser");
-		if (user == null) {
-			response.sendRedirect("/CheeseLoginServlet");
-			return;
-		}
+//		HttpSession session = request.getSession();
+//		CheeseUser user = (CheeseUser)session.getAttribute("loginUser");
+//		if (user == null) {
+//			response.sendRedirect("/CheeseLoginServlet");
+//			return;
+//		}
+//		// musicを作成したuserIdとログイン済みのuserIdが違うならログイン画面へ
+//		// musicIdの取得
+		int musicId = Integer.parseInt(request.getParameter("id"));
+//		// 曲データの取得
+//		CheeseMusicDao musicDao = new CheeseMusicDao();
+//		CheeseMusic music = musicDao.select(musicId);
+//		 // セッションスコープのuserIdとmusicのuserIdの比較
+//		if (user.getId() != music.getUserId()) {
+//			// 違うならログイン画面へ
+//			response.sendRedirect("/CheeseLoginServlet");
+//			return;
+//		}
 		
 		List<CheesePhrase> phraseList;
 		CheesePhraseDao phraseDao =  new CheesePhraseDao();
 		phraseList = phraseDao.select(new ArrayList<String>(), new ArrayList<String>(), "", 1);
 		request.setAttribute("phraseList", phraseList);
-		
-		// 曲編集画面にフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/cheese_edit_music.jsp");
-		dispatcher.forward(request, response);
-			
-		// musicを作成したuserIdとログイン済みのuserIdが違うならログイン画面へ
-		// musicIdの取得
-		int musicId = Integer.parseInt(request.getParameter("id"));
-		// 曲データの取得
-		CheeseMusicDao musicDao = new CheeseMusicDao();
-		CheeseMusic music = musicDao.select(musicId);
-		 // セッションスコープのuserIdとmusicのuserIdの比較
-		if (user.getId() != music.getUserId()) {
-			// 違うならログイン画面へ
-			response.sendRedirect("/CheeseLoginServlet");
-			return;
-		}
 			
 		CheeseMusicPhraseDao musicPhraseDao = new CheeseMusicPhraseDao();	
 		// music_phrase_tableからデータ取得
@@ -65,8 +57,17 @@ public class CheeseEditMusicServlet  extends HttpServlet {
 		// jspへセット
 		request.setAttribute("assignedMusicPhraseList", assignedMusicPhraseList);
 		request.setAttribute("assignedPhraseList", assignedPhraseList);
-				
-			
+		
+//		for (CheeseMusicPhrase m : assignedMusicPhraseList) {
+//			System.out.println(m.getId());
+//		}
+		for (CheesePhrase m : assignedPhraseList) {
+			System.out.println(m.getId());
+		}
+		
+		// 曲編集画面にフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/cheese_edit_music.jsp");
+		dispatcher.forward(request, response);
 				
 				
 //		// CheeseEditMusicServlet の doGet 内
