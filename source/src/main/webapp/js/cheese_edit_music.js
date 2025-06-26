639bb0138835e908cdcb37c5d99e08d42243e370
plusdojo2025-kikuura <kikuura-santaro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-26 10:31:01 +0900

    CheeseRegistMusicServletの修正

--- source/src/main/webapp/js/cheese_edit_music.js
+++ source/src/main/webapp/js/cheese_edit_music.js
@@ -189,6 +189,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// 最初の1個目をテンプレートとして使う
 	const templateBox = document.querySelector(".phraseBox");
+	templateBox.style.display = 'none';
 
 	// 枠の番号をふりなおす
 	function renumberBoxes() {
@@ -283,6 +284,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	// ＋ボタンが押された時の処理
 	addBtn.addEventListener("click", () => {
 		const clone = templateBox.cloneNode(true);
+		
+		clone.style.display = 'block';
 
 		// 入力初期化
 		const phrase_id = clone.querySelector('input[name="phrase_id"]');
